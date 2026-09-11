4a1df6cb4f8eba9068b61b1c4ddc6b2afd7d7582
salahmakboul <iuogh71@gmail.com>
2026-09-11 18:24:48 +0200

    Add: course-schedule-iv - submission-2

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        graph = {i:[] for i in range(numCourses)}
+        for a,b in prerequisites :
+            graph[a].append(b)
+        
+        result= {i:set() for i in range(numCourses)}
+
+        def dfs(current_node,start_node) :
+            neighbors = {u if current_node == v else v for u,v in graph if current_node in (u,v)}
+            for neighbor in neighbors :
+                if neighbor in result[start_node]:
+                    continue
+                result[start_node].append(neighbor)
+                dfs(current_node, start_node)
+        for i in range(numCourses):
+            dfs(i,i)
+        bolList=[]
+        for u,v in queries :
+            if v in result[u]:
+                bolList.append(True)
+            else :
+                bolList.append(False)
+        return bolList
