3f9392bc396adb6a2061618ab8b2c2bb137c5a0c
salahmakboul <iuogh71@gmail.com>
2026-09-21 21:34:53 +0200

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count =0
+        sted={0:1}
+        current_prefix=0
+
+        for i in range(len(nums)):
+            current_prefix+=nums[i]
+            complement = current_prefix - k
+            if complement in sted:
+                count+=1
+            if current_prefix in sted:
+                count += 1
+            else:
+                sted[current_prefix] = 1
+        
+        return count
+
+            
+            
+        
